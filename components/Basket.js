a63ba055cb92dd937f504221d6f1ae47d943deb5
AustenLeow <austenleow.al@gmail.com>
2022-07-27 12:03:03 +0800

    Delete components directory

--- components/Basket.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Basket(props) {
-    return <aside classname="block col-1">
-        <h2>cart items</h2>
-    </aside>
-}
--- components/cognito-api/UserPool.js
+++
@@ -1,9 +0,0 @@
-import { CognitoUserPool } from 'amazon-cognito-identity-js'
-
-const poolData = {
-  UserPoolId: "ap-southeast-1_MZb0CxZzA",
-  ClientId: "3ur5c8jclg5iki4j9tuvor5k74"
-
-}
-
-export default new CognitoUserPool(poolData)
