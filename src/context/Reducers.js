09b14803971ab6d3ff465db0860b1c3ef8a6de91
AustenLeow <austenleow.al@gmail.com>
2022-07-27 22:53:08 +0800

    Delete src directory

--- src/context/Reducers.js
+++
@@ -1,8 +0,0 @@
-export const cartReducer = (state, action) => {
-    switch (action.type) {
-        case "ADD_TO_CART":
-            return {...state,}
-        default:
-            return state;
-    }
-}
